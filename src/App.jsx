2fae21afa1c8729d6d0c1f788f1d753127c00aa1
Faris Lakota <faris.lakota@gmail.com>
2025-11-10 22:59:43 +0100

    coinpage stranice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import PageLayout from "./Components/PageLayout/pagelayout";
 import Coins from "./Pages/Coins/coins"; 
+import CoinPage from "./Pages/CoinPage/CoinPage";
 
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
     <Routes>
       <Route element={<PageLayout />}>
         <Route index element={<Coins />} />
-       
+        <Route path="coin/:uuid" element={<CoinPage />} />
         <Route path="*" element={<div>Not found</div>} />
       </Route>
     </Routes>
